22357799d73afe84ad136731c9774ce126eccdf9
wchai <Wenle.Chai@windriver.com>
2019-04-18 10:39:17 +0800

    Skip 2 test cases of test_pathlib for VxWorks
    Skip test_is_fifo_true(), because VxWorks mkfifo() has a limitation, the
    path of named pipe has to be started with '/fifos', create named pipes
    at directories from some test cases will be failed.
    
    Skip test_expanduser(), because the pwd.getpwduid() is partially supported
    by VxWorks, it can not:
      - Get group ID(pw_gid)
      - Get home directory(pw_dir)
      - Get shell program(pw_shell)
    Use pwd.getpwduid().pw_dir in the tests will be failed.

--- Lib/test/test_pathlib.py
+++ Lib/test/test_pathlib.py
@@ -1980,6 +1980,7 @@ class _BasePathTest(object):
         self.assertIs((P / 'fileA\x00').is_fifo(), False)
 
     @unittest.skipUnless(hasattr(os, "mkfifo"), "os.mkfifo() required")
+    @support.skip_if_restricted_mkfifo
     def test_is_fifo_true(self):
         P = self.cls(BASE, 'myfifo')
         try:
@@ -2214,6 +2215,8 @@ class PosixPathTest(_BasePathTest, unittest.TestCase):
 
     @unittest.skipUnless(hasattr(pwd, 'getpwall'),
                          'pwd module does not expose getpwall()')
+    @unittest.skipIf(sys.platform == "vxworks",
+                     "pwd module does not support getpwuid().pw_dir on VxWorks")
     def test_expanduser(self):
         P = self.cls
         support.import_module('pwd')
