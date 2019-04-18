3673d2b33b9181d933fab046e6e73ed909838aa4
wchai <wenle.chai@windriver.com>
2019-04-18 10:14:33 +0800

    Add support.skip_if_restricted_mkfifo() for VxWorks
    The VxWorks mkfifo() has a restriction: the path argument specified with
    the mkfifo() call must point to the path on the pipe device '/fifos'. So
    add a `skip_if_restricted_mkfifo()` method to skip the os.mkfifo()
    related tests for VxWorks.

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -94,10 +94,10 @@ __all__ = [
     "requires_IEEE_754", "skip_unless_xattr", "requires_zlib",
     "anticipate_failure", "load_package_tests", "detect_api_mismatch",
     "check__all__", "skip_unless_bind_unix_socket",
-    "ignore_warnings",
+    "ignore_warnings", "skip_if_restricted_mkfifo",
     # sys
     "is_jython", "is_android", "check_impl_detail", "unix_shell",
-    "setswitchinterval",
+    "setswitchinterval", "is_vxworks",
     # network
     "HOST", "IPV6_ENABLED", "find_unused_port", "bind_port", "open_urlresource",
     "bind_unix_socket",
@@ -817,6 +817,8 @@ requires_lzma = unittest.skipUnless(lzma, 'requires lzma')
 is_jython = sys.platform.startswith('java')
 
 is_android = hasattr(sys, 'getandroidapilevel')
+
+is_vxworks = (sys.platform == "vxworks")
 
 if sys.platform != 'win32':
     unix_shell = '/system/bin/sh' if is_android else '/bin/sh'
@@ -2584,6 +2586,19 @@ def detect_api_mismatch(ref_api, other_api, *, ignore=()):
     missing_items = set(m for m in missing_items
                         if not m.startswith('_') or m.endswith('__'))
     return missing_items
+
+
+def skip_if_restricted_mkfifo(test):
+    """Skip decorator for tests that require POSIX-defined mkfifo"""
+    msg = "Requires POSIX-defined mkfifo implementation"
+    """VxWorks mkfifo() has a restriction: the path argument specified
+    with the mkfifo() call must point to the path on the pipe device '/fifos'
+    """
+    if is_vxworks:
+        restricted = True
+    else:
+        restricted = False
+    return unittest.skip(msg)(test) if restricted else test
 
 
 def check__all__(test_case, module, name_of_module=None, extra=(),
