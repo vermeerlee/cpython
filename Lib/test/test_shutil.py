510e5a13fad64c41e1b133a73f5672d54fbbb122
wchai <Wenle.Chai@windriver.com>
2019-04-18 11:20:32 +0800

    Skip mkfifo() related test cases for VxWorks
    Skip test_copyfile_named_pipe() and test_copytree_named_pipe().
    
    The VxWorks mkfifo() has a restriction: the path argument specified with
    the mkfifo() call must point to the path on the pipe device '/fifos',
    create named pipes at directories from these 2 tests will be failed.

--- Lib/test/test_shutil.py
+++ Lib/test/test_shutil.py
@@ -683,6 +683,7 @@ class TestCopyTree(BaseTest, unittest.TestCase):
     # Issue #3002: copyfile and copytree block indefinitely on named pipes
     @unittest.skipUnless(hasattr(os, "mkfifo"), 'requires os.mkfifo()')
     @os_helper.skip_unless_symlink
+    @support.skip_if_restricted_mkfifo
     def test_copytree_named_pipe(self):
         os.mkdir(TESTFN)
         try:
@@ -1206,6 +1207,7 @@ class TestCopy(BaseTest, unittest.TestCase):
 
     # Issue #3002: copyfile and copytree block indefinitely on named pipes
     @unittest.skipUnless(hasattr(os, "mkfifo"), 'requires os.mkfifo()')
+    @support.skip_if_restricted_mkfifo
     def test_copyfile_named_pipe(self):
         try:
             os.mkfifo(TESTFN)
